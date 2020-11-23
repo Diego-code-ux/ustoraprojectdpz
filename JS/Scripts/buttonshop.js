0b5fed9a2b59593f96d8803f7504b06f32f44052
Diego-code-ux <74888399+Diego-code-ux@users.noreply.github.com>
2020-11-22 21:33:23 -0600

    Add files via upload

---
+++ JS/Scripts/buttonshop.js
@@ -0,0 +1,57 @@
+﻿$('.minus-btn').on('click', function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+
+    if (value > 1) {
+        value = value - 1;
+    } else {
+        value = 0;
+    }
+
+    $input.val(value);
+
+});
+
+$('.plus-btn').on('click', function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+
+    if (value < 100) {
+        value = value + 1;
+    } else {
+        value = 100;
+    }
+
+    $input.val(value);
+});
+
+$('.like-btn').on('click', function () {
+    $(this).toggleClass('is-active');
+});
+/*Botones +,- Del producto
+--------------------------------------------*/
+function increment() {
+    document.getElementById('demoInput').stepUp(1);
+}
+function decrement() {
+    document.getElementById('demoInput').stepDown(1);
+}
+/*Funciones de las tabs
+--------------------------------------------*/
+function desctab(evt, vbs) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(vbs).style.display = "block";
+    evt.currentTarget.className += " active";
+}
